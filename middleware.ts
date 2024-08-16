2844d73836b20833401e903805416d44143b6a0a
YuniqueUnic <2445942195@qq.com>
2024-08-17 00:46:23 +0800

    feat(dashboard): 添加邀请按钮和搜索输入框
    在dashboard中添加了一个邀请按钮和搜索输入框。邀请按钮允许用户邀请其他成员加入，搜索输入框允许用户搜索看板。
    
    fix(middleware): 保护所有路由
    
    修改了中间件设置，确保所有路由都受到保护。现在所有请求都需要经过身份验证，除非是登录和注册路由。
    
    refactor(org-sidebar): 重新设计组织边栏
    
    重新设计了组织边栏，使其更加用户友好，并添加了链接和组织切换器。这提高了用户在不同组织和看板之间的导航能力。
    
    feat(navbar): 添加搜索输入和邀请按钮
    
    在导航栏中添加了搜索输入框和邀请按钮，使用户能够快速搜索内容并邀请新成员。这增强了导航栏的功能性和用户体验。
    
    feat(input): 自定义输入组件
    
    实现了一个自定义输入组件，用于在应用程序中统一输入字段的样式和功能。这提高了输入控件的一致性和可维护性。

--- middleware.ts
+++ middleware.ts
@@ -4,15 +4,16 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 // So, according the docs of clerk middleware, we should make a list to store the route need to be protected.
 // Record from the official docs about nextjs: By default, clerkMiddleware will not protect any routes. All routes are public and you must opt-in to protection for routes.
 // Link: https://clerk.com/docs/references/nextjs/clerk-middleware
-// const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]); // Make all routes protected
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]); // Make all routes protected
 
-export default clerkMiddleware();
-// (auth, req) => {
-//   if (!isPublicRoute(req)) {
-//     auth().protect();
-//   }
-// },
-// { debug: true }
+export default clerkMiddleware(
+  (auth, req) => {
+    if (!isPublicRoute(req)) {
+      auth().protect();
+    }
+  }
+  // { debug: true }
+);
 
 export const config = {
   matcher: [
