8f2c29379e5c98cbab5b965bb45150e044e462ff
YuniqueUnic <2445942195@qq.com>
2024-08-16 22:05:34 +0800

    ``` feat(dashboard): 实现侧边栏组织项创建与切换
    添加了新组织创建按钮和组织切换功能。通过在侧边栏中整合提示工具和动态组织项，提高了用户交互性。
    确保Clerk的组织和组织列表Hooks正确集成以管理用户在不同组织间的导航和创建操作。
    ```

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -1,6 +1,41 @@
 "use client";
 
 import Image from "next/image";
-import { useOrganization, useOrganizationList } from "@clerk/clerk-react";
-
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { Hint } from "@/components/hint";
 import { cn } from "@/lib/utils";
+
+interface ItemsProps {
+  id: string;
+  name: string;
+  imageUrl: string;
+}
+
+export const Item = ({ id, name, imageUrl }: ItemsProps) => {
+  const { organization } = useOrganization();
+  const { setActive } = useOrganizationList();
+
+  const isActive = organization?.id === id;
+
+  const onClick = () => {
+    if (!setActive) return;
+
+    setActive({ organization: id });
+  };
+  return (
+    <div className="aspect-square relative">
+      <Hint label={name} sideOffset={18} side="right" align="center">
+        <Image
+          fill
+          src={imageUrl}
+          onClick={onClick}
+          alt={name}
+          className={cn(
+            "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
+    </div>
+  );
+};
---
+++ components/hint.tsx
@@ -0,0 +1,40 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+
+export interface HintProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  alignOffset?: number;
+}
+
+export const Hint = ({
+  label,
+  children,
+  side,
+  align,
+  sideOffset,
+  alignOffset,
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={1000}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent
+          className="text-white bg-black border-black rounded-md px-2 py-1"
+          side={side}
+          align={align}
+          sideOffset={sideOffset}
+          alignOffset={alignOffset}>
+          <p className="font-semibold capitalize">{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
