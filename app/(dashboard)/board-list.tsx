897b0a2216c0cef76aaf89eb6cbed171be0ded45
YuniqueUnic <2445942195@qq.com>
2024-08-19 16:31:32 +0800

    ``` refactor(dashboard): 创建空状态组件以提升可复用性
    引入新的空状态组件，以替换静态的空状态消息。现在，EmptyBoards、EmptyFavorites 和 EmptySearch 组件根据不同的情况显示相应的图像和信息，以提升代码的可复用性和维护性。
    ```

--- app/(dashboard)/board-list.tsx
+++ app/(dashboard)/board-list.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { EmptyBoards } from "./_components/empty-boards";
+import { EmptyFavorites } from "./_components/empty-favorites";
+import { EmptySearch } from "./_components/empty-search";
+
 interface BoardListProps {
   orgId: string;
   query: {
@@ -12,15 +16,15 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
   const data = []; // TODO:Change to the API Call
 
   if (!data?.length && query.search) {
-    return <div>Try searching for something else</div>;
+    return <EmptySearch />;
   }
 
   if (!data?.length && query.favorites) {
-    return <div>No favorites</div>;
+    return <EmptyFavorites />;
   }
 
   if (!data.length) {
-    return <div>No boards at all</div>;
+    return <EmptyBoards />;
   }
 
   return <div>{JSON.stringify(query)}</div>;
