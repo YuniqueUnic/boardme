3b450388d3937d8db505172ac78038293db3d952
YuniqueUnic <2445942195@qq.com>
2024-08-19 17:15:06 +0800

    feat(toasts): 集成sonner库以管理通知
    集成sonner库来处理应用中的通知。修改了layout.tsx以包含Toaster组件，并更新了EmptyBoards组件以使用新的通知逻辑。此外，添加了useApiMutation钩子来处理API调用的状态，包括pending状态的管理。
    
    - 在layout.tsx中添加Toaster组件，用于全局通知管理。
    - 重构EmptyBoards组件，实现当用户创建新看板时显示通知。
    - 创建useApiMutation自定义钩子，封装API调用，统一管理pending状态和错误处理。
    - 更新package.json和pnpm-lock.yaml以反映新增的依赖项：sonner和next-themes。

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,33 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import Image from "next/image";
+import { toast } from "sonner";
+import { useOrganization } from "@clerk/clerk-react";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization?.id,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board created");
+        // TODO: Redirect to board/{id}
+      })
+      .catch((e) => {
+        toast.error("Failed to create board");
+      });
+  };
+
   return (
     <div className="h-full flex flex-col justify-center items-center">
       <Image src={"/note.svg"} height={200} width={200} alt="Empty" />
@@ -10,7 +36,9 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button size={"lg"}>Create board</Button>
+        <Button disabled={pending} onClick={onClick} size={"lg"}>
+          Create board
+        </Button>
       </div>
     </div>
   );
